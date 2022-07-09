773a03835879599e1aee7d36bf6d04e4323f3ea9
TenzinJungney <jungney5@gmail.com>
2022-07-08 21:14:33 -0400

    javascript for menu toggle in script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-// Menu toggle 
+// PSEUDOCODE
+
+// Menu toggle
+    // when clicked on the burger menu icon, navigation page will slide from the right side.
+    // when clicked on the X icon, navigation page retreats away from the right side.
+
+// storing button elements in variables
+const menuToggle = document.querySelector('.menu-toggle');
+const closeButton = document.querySelector('.close-button');
+// storing .slide-out-nav in a variable
+const slideOutNav = document.querySelector('.slide-out-nav');
+// when clicked on the burger menu icon, navigation page will slide from the right side.
+menuToggle.addEventListener('click', function() {
+    slideOutNav.classList.toggle('show-slide-nav');
+})
+// when clicked on the X icon, navigation page retreats away from the right side.
+closeButton.addEventListener('click', function() {
+    slideOutNav.classList.toggle('show-slide-nav');
+})
+ 
 
 // Add Comment

5e34db56de894072a6a39389d1fc5eddbe14bab8
TenzinJungney <jungney5@gmail.com>
2022-07-11 23:02:29 -0400

    been trying to store live date and display it with the corresponding comment

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ closeButton.addEventListener('click', function() {
 })
  
 
-// Add Comment
+// Add/DISPLAY Comment from USER Input
     // after user submits comment:
     // 1. the comment input gets added after the last comment.
 
@@ -30,12 +30,12 @@ const commentSection = document.getElementById('comment-section');
 // Listen for the submit on the form
 formEl.addEventListener('submit', function(event) {
     event.preventDefault();
-    // console.log(event);
     // store the textarea element in a variable
     const textareaEl = document.querySelector('textarea');
     // get the value of the value property and save it in a variable 
-    // console.log(textareaEl.value);
     const comment = textareaEl.value;
+    // store the timeStamp value in a variable
+    const date = event.timeStamp;
 
     if (comment) {
         console.log(comment);
@@ -44,22 +44,20 @@ formEl.addEventListener('submit', function(event) {
         const articleEl = document.createElement('article');
         articleEl.classList.add('comment-post');
         // create div element
-        const divEl = document.createElement('div');
-        const div1 = divEl;
+        const div1 = document.createElement('div');
         div1.classList.add('comment-image');
         // create img element
         const imgEl = document.createElement('img');
         console.log(imgEl);
         imgEl.src = "https://source.unsplash.com/random/80x80";
 
-        const div2 = divEl;
+        const div2 = document.createElement('div');
         div2.classList.add('comment');
         // create a p element
-        const paraEl = document.createElement('p');
-        const paraDate = paraEl;
-        
-        const paraComment = paraEl;
+        const paraDate = document.createElement('p');
 
+        const paraComment = document.createElement('p');
+        
         // add the div1 element in the artile element
         articleEl.appendChild(div1);
 
@@ -68,6 +66,12 @@ formEl.addEventListener('submit', function(event) {
 
         // add another div to the article element
         articleEl.appendChild(div2);
+
+        // update paraDate with the timeStamp
+        paraDate.textContent = date;
+
+        // add paraDate to the div2
+        articleEl.appendChild(div2).appendChild(paraDate);
         
         // update the paragraph text with the user comment submit
         paraComment.textContent = comment;
@@ -77,6 +81,11 @@ formEl.addEventListener('submit', function(event) {
         
         // append the article to the section page
         document.querySelector('#comment-section').appendChild(articleEl);
-        
+
+        // clearing our input by setting it back to an empty string
+        textareaEl.value = '';
+    }
+    else {
+        alert("please write some comment.");
     }
 })
